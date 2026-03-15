b09ad11e014008993dcd1f0ab6d955d2e7ee8b64
Louts21 <72473132+Louts21@users.noreply.github.com>
2026-03-15 23:17:17 +0100

    removed welcome page and added configuration page

---
+++ app/configuration/configuration.tsx
@@ -0,0 +1,7 @@
+export function Configuration() {
+    return (
+        <div className="flex flex-col items-center justify-center gap-4">
+            <h1 className="text-2xl font-bold">Powerconfigurator</h1>
+        </div>
+    );
+}
